94dd3e2d407ab1a2b2dce90a6baa8e6292391226
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2017-02-28 01:08:21 -0500

    Comentario  de Bloque

--- js/archivoExterno.js
+++ js/archivoExterno.js
@@ -1,4 +1,10 @@
 //JavaScript document
 //Nuestra primera pagina de JavaScript
+/*
+Realizado por: Anthony
+El dia: 28 de febrero 2017 Martes a la 1:07
+Clima: una noche hermosa
+Canción: We Will Rock You
+*/
 document.write("Hola Mundo JS / ");
 alert("Hola, cara de bola");
